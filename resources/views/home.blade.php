1c6343b33ae776c40dcb70ea0fcb37c8bc5e5b18
zaynasheff <zaynasheff@gmail.com>
2021-07-22 16:52:49 +0300

    ajax percent

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -191,7 +191,7 @@
                                                 var percent = ((response.countSuccess/response.count)*100).toFixed(2);
                                                 $('#line_processed_percent').text(' ('+percent+'%)');
                                                 console.log(response);
-                                                if(parseInt(response.countSuccess) + parseInt(response.countError) === parseInt(response.count - 1)){
+                                                if(parseInt(response.countSuccess) + parseInt(response.countError) === parseInt(response.count)){
                                                     location.href='{{route('home')}}'
                                                 }
 
